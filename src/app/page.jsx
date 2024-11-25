83f538c5f9448589ef83d37fedb6232f4ed7b0c8
harshalgupta7 <writetoharshalgupta@gmail.com>
2024-11-26 00:49:24 +0530

    init 3

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,17 @@
+import Searchbox from "@/components/item1/searchbox";
+import WeatherInfo from "@/components/item1/WeatherInfo";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div >
-      <div className="bg-black w-full h-[800px]">
-      
+      <div className="bg-white w-full h-[800px] m-4">
+
+        <div className="w-[97%] h-[700px] bg-slate-600">
+          <Searchbox />
+          <WeatherInfo />
+        </div>
+
       </div>
     </div>
   );
