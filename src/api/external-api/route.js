ce553b1cae83b44a57d98b3cbea923ee77fd3624
mukul-pixel <mukul8756@gmail.com>
2024-11-28 23:26:23 +0530

    api setup done

---
+++ src/api/external-api/route.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const fetchWeatherData = async () => {
+  const apiKey = process.env.NEXT_PUBLIC_WEATHER_KEY;
+  const apiURL = process.env.NEXT_PUBLIC_WEATHER_API_URL;
+
+  console.log({ apiURL, apiKey }); // Debugging log
+
+  if (!apiKey || !apiURL) {
+    console.error("API key or URL is not defined in the environment variables.");
+    return;
+  }
+  const params = {
+    lat: 28.7041,
+    lon: 77.1025,
+    units: "metric",
+    appid: apiKey,
+  };
+
+  try {
+    const response = await axios.get(apiURL,{params});
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+  }
+};
+
+fetchWeatherData();
+
+export default fetchWeatherData;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
+import fetchWeatherData from '@/api/external-api/route'
 
 const Header = () => {
+  useEffect(()=>{
+    const fetchData = async () => {
+      await fetchWeatherData();
+    };
+    fetchData();
+  },[])
   return (
     <div>Header</div>
   )
